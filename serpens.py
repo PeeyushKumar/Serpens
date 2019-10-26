0fb13c70ad2b56577aaa649ced4d56b9b2321dbb
Peeyush Kumar <peeyshkumar.25oct@gmail.com>
2019-10-26 18:24:17 +0530

    added body and it's behaviour

--- serpens.py
+++ serpens.py
@@ -2,6 +2,9 @@ import turtle
 import time
 import random
 
+body = []
+
+
 # Setting up the screen
 win = turtle.Screen()
 win.setup(width=600, height=400)
@@ -17,6 +20,7 @@ head.speed(0)
 head.penup()
 head.goto(0,0)
 head.direction = "stop"
+body.append(head)
 
 #Creating serpent food
 food = turtle.Turtle()
@@ -25,7 +29,7 @@ food.color("red")
 food.shapesize(1)
 food.speed(0)
 food.penup()
-food.goto(0,0)
+food.goto(random.randint(-280, +280),random.randint(-180, +180))
 food.direction = "stop"
 
 # Head movement
@@ -34,16 +38,16 @@ def move():
 	y = 0
 	if head.direction == "up":
 		y = head.ycor()
-		head.sety(y + 5)
+		head.sety(y + 15)
 	if head.direction == "down":
 		y = head.ycor()
-		head.sety(y - 5)
+		head.sety(y - 15)
 	if head.direction == "left":
 		x = head.xcor()
-		head.setx(x - 5) 
+		head.setx(x - 15) 
 	if head.direction == "right":
 		x = head.xcor()
-		head.setx(x + 5)
+		head.setx(x + 15)
 
 def goup():
 	if head.direction != "down":
@@ -68,14 +72,28 @@ win.onkeypress(goright, "Right")
 # Main loop
 while True:
 	#Serpent eating food
-	if head.distance(food) < 20:
+	if head.distance(food) < 15:
 		x = random.randint(-280, +280)
 		y = random.randint(-180, +180)
 		food.goto(x,y)
 
+		#Add a new part
+		part = turtle.Turtle()
+		part.penup()
+		part.speed(0)
+		part.shape("square")
+		part.color("grey")
+		part.direction = "stop"
+		body.append(part)
+		
+	#Body movement
+	for index in range(len(body)-1,0,-1):
+		body[index].setx(body[index-1].xcor())
+		body[index].sety(body[index-1].ycor())
+
 
 	move()
 	win.update()
-	time.sleep(0.03)
+	time.sleep(0.05)
 
 turtle.mainloop()
