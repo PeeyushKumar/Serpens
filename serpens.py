5481186909583976b0d361724d19de8fb475015a
Peeyush Kumar <peeyshkumar.25oct@gmail.com>
2019-10-26 16:56:35 +0530

    added head movement

--- serpens.py
+++ serpens.py
@@ -11,15 +11,53 @@ win.tracer(0)
 head = turtle.Turtle()
 head.shape("square")
 head.color("black")
-head.shapesize(1,1)
+head.shapesize(1)
 head.speed(0)
 head.penup()
 head.goto(0,0)
 head.direction = "stop"
 
+# Head movement
+def move():
+	x = 0
+	y = 0
+	if head.direction == "up":
+		y = head.ycor()
+		head.sety(y + 5)
+	if head.direction == "down":
+		y = head.ycor()
+		head.sety(y - 5)
+	if head.direction == "left":
+		x = head.xcor()
+		head.setx(x - 5) 
+	if head.direction == "right":
+		x = head.xcor()
+		head.setx(x + 5)
 
+def goup():
+	if head.direction != "down":
+		head.direction = "up"
+def godown():
+	if head.direction != "up":
+		head.direction = "down"
+def goleft():
+	if head.direction != "right":
+		head.direction = "left"
+def goright():
+	if head.direction != "left":
+		head.direction = "right"
+
+win.listen()
+win.onkeypress(goup, "Up")
+win.onkeypress(godown, "Down")
+win.onkeypress(goleft, "Left")
+win.onkeypress(goright, "Right")
+
+
+# Main loop
 while True:
 	win.update()
-	time.sleep(0.1)
+	time.sleep(0.03)
+	move()
 
 turtle.mainloop()
