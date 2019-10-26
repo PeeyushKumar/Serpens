4b73d7a4b2ecbdc63175f8280105e8ce28f13021
Peeyush Kumar <peeyshkumar.25oct@gmail.com>
2019-10-26 16:08:44 +0530

    created serpent head and adding mainloop

--- serpens.py
+++ serpens.py
@@ -1,12 +1,25 @@
 import turtle
-
+import time
 
 # Setting up the screen
 win = turtle.Screen()
-win.setup(600,600)
+win.setup()
 win.title("Serpens")
 win.tracer(0)
 
+# Creating serpent head
+head = turtle.Turtle()
+head.shape("square")
+head.color("black")
+head.shapesize(1,1)
+head.speed(0)
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
 
+
+while True:
+	win.update()
+	time.sleep(0.1)
 
 turtle.mainloop()
